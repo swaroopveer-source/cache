22bf86a9921d8c60788f60a54fe27d8c1c65883e
swaroopveer-source <swaroop.veer@runpod.io>
2026-07-15 15:00:00 +0530

    Update runpod_helper.py

--- docker/worker/runpod_helper.py
+++ docker/worker/runpod_helper.py
@@ -1,9 +1,18 @@
+import runpod
+
 from app.version import VERSION
 from modules.helper import get_message
 
-print("Worker Started")
-print(VERSION)
-print(get_message())
 
-while True:
-    pass
+def handler(event):
+    input_data = event.get("input", {})
+
+    return {
+        "success": True,
+        "version": VERSION,
+        "message": get_message(),
+        "input": input_data
+    }
+
+
+runpod.serverless.start({"handler": handler})
